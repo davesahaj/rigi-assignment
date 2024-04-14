e163b19e6f5ebdf71fee78d1acafc40e7443a201
Dave <sahajdavework@pm.me>
2024-04-14 18:36:56 +0530

    Added: Global API Provider, Renamed route dir

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { RouterProvider, ThemeProvider } from './providers';
+import { ApiProvider, RouterProvider, ThemeProvider } from './providers';
 
 import './global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider />
+      <ApiProvider>
+        <RouterProvider />
+      </ApiProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
