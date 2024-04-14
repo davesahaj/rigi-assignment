a9699c25898c2ebbd171061ca38d9e4e385047cd
Dave <sahajdavework@pm.me>
2024-04-14 17:41:09 +0530

    Refactor: Theme and Routes integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { RouterProvider, ThemeProvider } from './providers';
+
 import './global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>test</React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider />
+    </ThemeProvider>
+  </React.StrictMode>
 );
