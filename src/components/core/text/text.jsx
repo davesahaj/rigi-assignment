c9971b02d10055fe9dc6c1faf70a13172d2374d0
Dave <sahajdavework@pm.me>
2024-04-14 17:03:52 +0530

    prettier fix

--- src/components/core/text/text.jsx
+++ src/components/core/text/text.jsx
@@ -1,10 +1,10 @@
 const textStyles = {
-    heading: 'text-4xl font-bold',
-    subheading: 'text-xl font-semibold font-urbanist text-emerald-500',
-    regular: 'text-base font-normal font-lato',
-    thin: 'text-base font-thin',
-}
+  heading: 'text-4xl font-bold',
+  subheading: 'text-xl font-semibold font-urbanist text-emerald-500',
+  regular: 'text-base font-normal font-lato',
+  thin: 'text-base font-thin'
+};
 
 export const Text = ({ children, type = textStyles.regular, className }) => {
-    return <div className={`${textStyles[type]} ${className}`}>{children}</div>
-}
+  return <div className={`${textStyles[type]} ${className}`}>{children}</div>;
+};
