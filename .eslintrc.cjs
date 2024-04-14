785627734a748cc32393922abbb3c97f7b537dbd
Dave <sahajdavework@pm.me>
2024-04-14 17:03:08 +0530

    Update: ESLint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,42 +2,43 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier'
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh", "simple-import-sort"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'simple-import-sort'],
   rules: {
-    "react/jsx-no-target-blank": "off",
-    "simple-import-sort/imports": [
-      "error",
+    'react/jsx-no-target-blank': 'off',
+    'react/prop-types': 'off',
+    'simple-import-sort/imports': [
+      'error',
       {
         groups: [
           // Packages `react` related packages come first.
-          ["^react", "^@?\\w"],
-          ["^next", "^@?\\w"],
+          ['^react', '^@?\\w'],
+          ['^next', '^@?\\w'],
           // Internal packages.
-          ["^(@|components)(/.*|$)"],
+          ['^(@|components)(/.*|$)'],
           // Side effect imports.
-          ["^\\u0000"],
+          ['^\\u0000'],
           // Parent imports. Put `..` last.
-          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
           // Other relative imports. Put same-folder imports and `.` last.
-          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
           // Style imports.
-          ["^.+\\.?(css)$"],
-        ],
-      },
+          ['^.+\\.?(css)$']
+        ]
+      }
     ],
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-  },
+    'react-refresh/only-export-components': [
+      'off',
+      { allowConstantExport: true }
+    ]
+  }
 };
